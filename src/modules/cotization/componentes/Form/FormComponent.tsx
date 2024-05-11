6f3efce9ffb0885c3dc393d537ddada0d0764562
Aaron Gaspar <gaspar_aaron@hotmail.com>
2024-05-10 22:42:37 -0500

    feat: logic button change currency and amount

--- src/modules/cotization/componentes/Form/FormComponent.tsx
+++ src/modules/cotization/componentes/Form/FormComponent.tsx
@@ -1,16 +1,27 @@
 import { useSelector } from 'react-redux';
 import './FormComponent.css';
+import { useEffect, useRef, useState } from 'react';
 
 function FormComponent () {
     const rates = useSelector((state:any) => state.user);
     const {sale_price,purchase_price} = rates;
+    const [activate,setActivate] = useState(true);
+    const [activate2,setActivate2] = useState(false);
+
+    const [soles, setSoles] = useState('');
+    const [dolar, setDolar] = useState('');
 
     const purchasePrice = () => {
-        console.log('purchasePrice')
+        setActivate(true)
+        setActivate2(false)
+        const purchasePrice = Number(purchase_price)
+
       };
     
     const salePrice = () => {
-        console.log('salePrice')
+        setActivate(false)
+        setActivate2(true)
+        const salesPrice = Number(sale_price)
       };
     
     const initOperation = () => {
@@ -18,27 +29,70 @@ function FormComponent () {
       };
 
     const updateOperation = () => {
-        console.log('updateOperation')
+        updateCurrency();
     };
 
+    const updateCurrency = () => {
+        if(activate2 === false ){
+            setActivate2(true)
+            setActivate(false)
+        } else {
+            setActivate2(false)
+            setActivate(true)
+        }
+    }
+
+    const onchangeHandler = (e:any) => {
+        console.log(e.target.value)
+        //setSoles(soles*Number(purchase_price));
+        // setDolar(dolar/Number(sale_price));
+    }
     return (
         <div className="containerForm">
             <div className='price'>
-            <button onClick={purchasePrice} className='purchasePrice'>
-                <p className='textButtonCardList'>Dolár venta {purchase_price}</p>
+            <button onClick={purchasePrice} className={activate ? 'purchasePrice':'purchasePriceDisabled'} disabled={activate}>
+                <p className='textButtonCardList'>Dolár compra {purchase_price}</p>
             </button>
-            <button onClick={salePrice} className='salePrice'>
+
+            <button onClick={salePrice} className={activate2?'salePrice':'salePriceDisabled'} disabled={activate2}>
                 <p className='textButtonCardList'>Dolár venta {sale_price}</p>
+                
             </button>
             </div>
+
             <div className='formPrice'>
-                <input className='inputSol' type='text' />
+                {/* <input 
+                className={activate ? 'inputDollar': 'inputSol'} 
+                type='number'
+                name = 'dolar' 
+                placeholder='Dolares'
+                value={value.soles}
+                onChange={onchangeHandler}
+                /> */}
+
+                <input 
+                className={activate ? 'inputDollar': 'inputSol'} 
+                type='number'
+                onChange={onchangeHandler}
+                />
                 <div className='updateOperation'>
                 <button onClick={updateOperation} className='buttonCardList'>
-                <img src="Group 1370.svg"/>                
+                <img src="Group 1370.svg"/>              
                 </button>
                 </div>
-                <input className='inputDollar' type='text' />
+                {/* <input 
+                className={activate2 ? 'inputDollar': 'inputSol'} 
+                type='number'
+                name = 'soles'  
+                placeholder='Soles'
+                value={dolar}  
+                onChange={onchangeHandler}
+                /> */}
+                <input 
+                className={activate2 ? 'inputDollar': 'inputSol'} 
+                type='number'
+                onChange={onchangeHandler}
+                />
             </div>
             <div className='buttonFooter'>
             <button onClick={initOperation} className='buttonOperationInit'>
